14383fe97459f52150ff4bea87b580289e1ad9f8
sang-w0o <robbyra@gmail.com>
2021-09-30 14:46:47 +0900

    feat: Define domain models

--- src/main/kotlin/com/example/domain/ItemRepository.kt
+++ src/main/kotlin/com/example/domain/ItemRepository.kt
@@ -1,4 +1,7 @@
 package com.example.domain
 
-interface ItemRepository {
-}
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ItemRepository : ReactiveMongoRepository<Item, String>
